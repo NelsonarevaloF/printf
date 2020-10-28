a6695526a87b540a38be9d0a9306c2d7525aac1c
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-28 00:10:00 -0500

    0x11. C - printf print_s.c

--- print_s.c
+++ print_s.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 /**
- * print_all - prints anything
- * @format: list of types or arguments to be passed
+ * print_s - prints strings
+ * @list: list of types or arguments to be passed
+ * @count: counts char printed
  * Return: void
  */
 void print_s(va_list list, int *count)
 {
-  char *str;
-  int i = 0;
+char *str;
+int i = 0;
 
-  str = va_arg(list, char *);
-  if (!str)
-    str = "(null)";
-  while (str[i])
-    {
-      _putchar(str[i], count);
-      i++;
-    }
+str = va_arg(list, char *);
+if (!str)
+str = "(null)";
+while (str[i])
+{
+_putchar(str[i], count);
+i++;
+}
 }
